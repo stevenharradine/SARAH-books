77327a7082eae2193a6b2d32b3ce487bf3a738d9
Steven Harradine <stevenharradine@gmail.com>
2015-09-20 23:37:58 -0400

    start read of appache dir

--- scripts/books-updater.js
+++ scripts/books-updater.js
@@ -1,49 +1,37 @@
+var request      = require('request'),
+    Cheerio      = require('cheerio'),
+    url          = "http://192.168.1.2/media/Music/Library/Books/",
+    books        = new Array (),
+    books_index  = 0,
+    agentOptions = {
+    	  url: url,
+    	  strictSSL: false	// allow self-signed
+    }
+
 console.log ("Books updater");
 console.log ("*************");
 console.log ();
 
 String.prototype.endsWith = function (suffix) {
-	return this.indexOf(suffix, this.length - suffix.length) !== -1;
-};
-
-var request = require('request');
-
-var url = "http://localhost/media/Music/Library/Books/";
-var url = "http://localhost/media/Music/Library/Books/william%20shakespeare/William%20Shakespeare/Hamlet%20(1891)/";
-var books = new Array ();
-var books_index = 0;
-
-function getTitle (url) {
-	url = "http://localhost/SARAH/apps/books/api/getTitle/?myusername=books_user&mypassword=books_password&book_url=" + url;
-	request(url, function (error, response, body) {
-		console.log (body);
-	});
+	return this.indexOf(suffix, this.length - suffix.length) !== -1
 }
 
-function getBooks (url) {
-	request(url, function (error, response, body) {
-		//if (!error && response.statusCode == 200) {
-		body_peices = body.split ('<a href="');
+var i = 0;
 
-		for (
-			i = 3;							// skip the headers
-			i < body_peices.length - 1;		// loop through the rest of the elements
-			i += 2							// because we pivit on eachside of
-											// the anchor tag we need to skip
-		) {									// every other element
-			secondary_fragments = body_peices[i + 1].split ('">');
-			link = secondary_fragments[0];
+request(agentOptions, function (error, response, body) {
+	if (error) {
+		return console.log (error)
+	}
+  if (!error && response.statusCode == 200) {
+    dom = Cheerio.load(body);
 
-			if (link.endsWith(".epub")) {							// if supported book format
-				book_url = url + link;
-				console.log (books[books_index++] = book_url);
-
-				getTitle (book_url);
-			} else if (link.endsWith ("/") && link.length >= 2) {	// if folder
-				getBooks (url + link);
-			}
-		}
-	});
-}
-
-getBooks (url);
+    
+		dom("table").find("tr").each(function(i, elem) {
+			dom(this).find("td").each (function(j, elem) {
+				if (j == 1) {
+					console.log (i++ + "|" + dom(this).find("a").attr("href"))
+				}
+			})
+		})
+  }
+})
